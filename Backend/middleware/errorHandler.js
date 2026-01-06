4568e3814aacc366bce6458ebfdf38075fd619b6
Atharva Butte <atharvabutte03@gmail.com>
2026-01-06 18:38:31 +0530

    Globar Error ahndler done

--- Backend/middleware/errorHandler.js
+++ Backend/middleware/errorHandler.js
@@ -8,10 +8,22 @@ export function globalErrorHandler(err,req,res,next) {
     console.log(err);
     let error = {...err}
     
+    // Manually copy beacuse some times spred does not copy them
     error.name = err.name;
-    error.message = err.message
-    
-    
-    
+    error.message = err.message;
+    error.code = err.code
+
+    if(error.name ==='JsonWebTokenError'){
+        error = handleJWTError();
+    }
+    if (error.name === 'TokenExpiredError') {
+        error= handleJWTTokenExpireError()
+    }
+    if(error.code){
+        console.log(`DB error code: ${error.code}`);
+        console.log(`DB error detail: ${error.detail}`);
+    }
+    const statusCode = error.statusCode || 500;
+    const message = error.isOperational? error.message : "Internal Server Error";
     res.status(statusCode).json({message});
 }
